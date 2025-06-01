8cc7ead4cc542365af7bbb1440d57bea7ea53f48
Guiltybench <alanchiquete9780@gmail.com>
2025-06-01 14:34:42 -0700

    not contact

--- data/moves.ts
+++ data/moves.ts
@@ -13162,7 +13162,7 @@ export const Moves: import('../sim/dex-moves').MoveDataTable = {
 		name: "New Moon",
 		pp: 10,
 		priority: 0,
-		flags: { contact: 1, protect: 1, mirror: 1, metronome: 1 },
+		flags: { protect: 1, mirror: 1, metronome: 1 },
 		onHit(pokemon) {
 			if (pokemon.getAbility().flags['cantsuppress']) {
 				return null;
