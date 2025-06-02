14a166baba257b0a93bf0cd3d9e32a0f62ff9c3d
Guiltybench <alanchiquete9780@gmail.com>
2025-06-01 19:35:04 -0700

    Update abilities.ts

--- data/abilities.ts
+++ data/abilities.ts
@@ -2914,18 +2914,16 @@ export const Abilities: import('../sim/dex-abilities').AbilityDataTable = {
 		num: 256,
 	},
 	feastorfamine: {
-		onFoeAfterBoost(boost, target, source, effect) {
+		onFoeTryBoost(boost, target, source, effect) {
 			if (effect?.name === 'Opportunist' || effect?.name === 'Mirror Herb') return;
 			if (!this.effectState.boosts) this.effectState.boosts = {} as SparseBoostsTable;
 			const boostPlus = this.effectState.boosts;
 			let i: BoostID;
 			for (i in boost) {
-				if (boost[i]! > 0) {
+				if (boost[i]! 	> 0) {
 					boostPlus[i] = (boostPlus[i] || 0) + boost[i]!;
 				}
 			}
-			target.clearBoosts();
-			this.add('-clearboost', target);
 		},
 		onAnySwitchInPriority: -3,
 		onAnySwitchIn() {
