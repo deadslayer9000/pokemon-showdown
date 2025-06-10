baf8be18ab1bfde2cb5b00b43bc0859970f409e4
Guiltybench <alanchiquete9780@gmail.com>
2025-06-09 20:12:39 -0700

    thank bari for test branches

--- data/moves.ts
+++ data/moves.ts
@@ -3338,7 +3338,7 @@ export const Moves: import('../sim/dex-moves').MoveDataTable = {
 		accuracy: 100,
 		basePower: 85,
 		category: "Physical",
-		name: "Crimson Charge"
+		name: "Crimson Charge",
 		pp: 10,
 		priority: 0,
 		flags: {contact: 1, protect: 1, mirror: 1, metronome: 1, },
