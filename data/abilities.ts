d900911c9ce36ad2e42837d0f73083917a47f1d1
CreationQ312 <brospondek04@gmail.com>
2025-07-23 13:53:46 +0200

    bbond fix omegagren

--- data/abilities.ts
+++ data/abilities.ts
@@ -492,7 +492,7 @@ export const Abilities: import("../sim/dex-abilities").AbilityDataTable = {
 				source.formeChange("Greninja-Ash", this.effect, true);
 			}
 			if (
-				source.species.id === "omegagreninjabond" &&
+				(source.species.id === "omegagreninjabond" || (source.species.id === "omegagreninja" && source.ability === "battlebond" )) &&
 				source.hp &&
 				!source.transformed &&
 				source.side.foePokemonLeft()
