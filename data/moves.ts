2155177f79c0f8d92440026eeccdde3dcf53c79b
CreationQ312 <brospondek04@gmail.com>
2025-07-23 14:59:25 +0200

    foam frenzy fix

--- data/moves.ts
+++ data/moves.ts
@@ -7896,8 +7896,14 @@ export const Moves: import("../sim/dex-moves").MoveDataTable = {
 							"[from] move: Foam Frenzy",
 							`[of] ${pokemon}`
 						);
-						this.boost({ spa: 1 }, pokemon, pokemon, move);
+						pokemon.abilityState.foamfrenzy = true;
+						
 					}
+				}
+				if (pokemon.abilityState.foamfrenzy = true){
+					this.boost({ spa: 1 }, pokemon, pokemon, move);
+					this.hint(`Foam Frenzy boosted ${pokemon.name}'s Special Attack!`)
+					pokemon.abilityState.foamfrenzy = false;
 				}
 			}
 		},
