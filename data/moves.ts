5d15deb8253f358180883806dc420266a43b1e20
Guiltybench <alanchiquete9780@gmail.com>
2025-07-22 21:08:49 -0700

    QUICKPATCH

--- data/moves.ts
+++ data/moves.ts
@@ -14409,14 +14409,10 @@ export const Moves: import('../sim/dex-moves').MoveDataTable = {
 		pp: 10,
 		priority: 0,
 		flags: { protect: 1, mirror: 1, metronome: 1 },
-		onTryImmunity(target) {
-			// Truant and Insomnia have special treatment; they fail before
-			// checking accuracy and will double Stomping Tantrum's BP
-			if (target.ability === 'truant' || target.ability === 'insomnia') {
-				return false;
-			}
-		},
 		onHit(pokemon) {
+			if (pokemon.ability === 'truant' || pokemon.ability === 'insomnia') {
+				return null;
+			}
 			if (pokemon.getAbility().flags['cantsuppress']) {
 				return null;
 			}
@@ -22909,12 +22905,10 @@ export const Moves: import('../sim/dex-moves').MoveDataTable = {
 		pp: 5,
 		priority: 0,
 		flags: { protect: 1, mirror: 1, metronome: 1 },
-		onTryImmunity(target) {
-			if (target.ability === 'truant' || target.ability === 'levitate') {
-				return false;
-			}
-		},
 		onHit(pokemon) {
+			if (pokemon.ability === 'truant' || pokemon.ability === 'levitate') {
+				return null;
+			}
 			if (pokemon.getAbility().flags['cantsuppress']) {
 				return null;
 			}
