117678eac3a3dd0dc715314d7b2087958bc9b08e
CreationQ312 <brospondek04@gmail.com>
2025-07-28 19:08:14 +0200

    msg test

--- data/moves.ts
+++ data/moves.ts
@@ -7486,7 +7486,7 @@ export const Moves: import("../sim/dex-moves").MoveDataTable = {
 					bp + (10 * this.field.pseudoWeather.echoedvoice.multiplier - 10)
 				);
 			}
-			this.hint(`Flashpoint's BP: ${bp}`);
+			console.log(`Flashpoint's BP: ${bp}`);
 			return bp;
 		},
 		category: "Physical",
