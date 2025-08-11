35dc149a51bbf77c3d67e8e33c6d04ed41128d0f
CreationQ312 <brospondek04@gmail.com>
2025-08-11 16:53:59 +0200

    color format test4

--- data/rulesets.ts
+++ data/rulesets.ts
@@ -41,7 +41,7 @@ export const Rulesets: import('../sim/dex-formats').FormatDataTable = {
 				
 			}
 		},
-	},
+	
 	// Regular Rulesets
 	///////////////////////////////////////////////////////////////////
 
