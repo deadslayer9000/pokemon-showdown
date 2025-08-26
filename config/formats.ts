0d80b5c2403aa45c38ae6c3f77df81872d7d480f
CreationQ312 <brospondek04@gmail.com>
2025-08-27 00:45:35 +0200

    delta vgc test

--- config/formats.ts
+++ config/formats.ts
@@ -39,12 +39,12 @@ export const Formats: import('../sim/dex-formats').FormatList = [
 		],
 	},
 	{
-		name: "[Gen 9] VGC Delta Reg Beta",
+		name: "[Gen 9] [ND] Delta VGC Reg Beta",
 		mod: 'gen9',
-		bestOfDefault: true,
-		ruleset: ['Flat Rules', '!! Adjust Level = 50', 'Min Source Gen = 1', 'VGC Timer', 'Standard NatDex', 'Terastal Clause'],
+		gameType: 'doubles',
+		ruleset: ['Flat Rules', '!! Adjust Level = 50', 'Min Source Gen = 1', 'VGC Timer', 'Standard NatDex', 'Terastal Clause', 'Open Team Sheets'],
 		restricted: ['Mythical'],
-		banlist: [ 'Alakazam-Mega', 'Magirigus', 'Slowhost', 'Maestrot', 'Melmetal', 'Magearna', 'Mewtwo', ],
+		banlist: [ 'Alakazam-Mega', 'Magirigus', 'Slowhost', 'Maestrot', 'Melmetal', 'Magearna', 'Mewtwo', 'Eevium Z'],
 		unbanlist: ['Flutter Mane', 'Iron Bundle', 'Chi-Yu','Chien-Pao', 'Urshifu-Single-Strike', 'Urshifu-Rapid-Strike', 'Urshifu', 'Gholdengo', 'Ogerpon-Hearthflame', 'Ogerpon-Shadowcrest', 'Naganadel', 'Blastoise-Mega', 'Gouging Fire', 'Walking Wake', 'Lucario-Mega', 'Landorus-Incarnate', 'Pheromosa', 'Dragapult', 'Terapagos'],
 	},
 	{
