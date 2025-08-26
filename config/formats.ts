412b5688691f51c6ec9951b5de5cd2139f4b8a3e
CreationQ312 <brospondek04@gmail.com>
2025-08-27 01:29:25 +0200

    delta vgc test

--- config/formats.ts
+++ config/formats.ts
@@ -45,7 +45,7 @@ export const Formats: import('../sim/dex-formats').FormatList = [
 		ruleset: ['Flat Rules', '!! Adjust Level = 50', 'Min Source Gen = 1', 'VGC Timer', 'Standard NatDex', 'Terastal Clause', 'Open Team Sheets'],
 //		restricted: ['Mythical'],
 		banlist: [ 'Alakazam-Mega', 'Magirigus', 'Slowhost', 'Maestrot', 'Melmetal', 'Magearna', 'Mewtwo', 'Eevium Z', 'Whirling Abyss', 'Rising Phoenix', 'Iron Storm', 'Iron Revival', 'Gengar-Mega', 'Kangaskhan-Mega', 'Deoxys', 'Deoxys-Attack', 'Dark Void'],
-		unbanlist: ['Flutter Mane', 'Iron Bundle', 'Chi-Yu','Chien-Pao', 'Urshifu-Single-Strike', 'Urshifu-Rapid-Strike', 'Urshifu', 'Gholdengo', 'Ogerpon-Hearthflame', 'Ogerpon-Shadowcrest', 'Naganadel', 'Blastoise-Mega', 'Gouging Fire', 'Walking Wake', 'Lucario-Mega', 'Landorus-Incarnate', 'Pheromosa', 'Dragapult', 'Terapagos', 'Mythical'],
+		unbanlist: ['Flutter Mane', 'Iron Bundle', 'Chi-Yu','Chien-Pao', 'Urshifu-Single-Strike', 'Urshifu-Rapid-Strike', 'Urshifu', 'Gholdengo', 'Ogerpon-Hearthflame', 'Ogerpon-Shadowcrest', 'Naganadel', 'Blastoise-Mega', 'Gouging Fire', 'Walking Wake', 'Lucario-Mega', 'Landorus-Incarnate', 'Pheromosa', 'Dragapult', 'Terapagos', 'Mythical', 'Deoxys-Defense', 'Deoxys-Speed'],
 	},
 	{
 		name: "[Gen 9] [ND] Realgam AAA",
