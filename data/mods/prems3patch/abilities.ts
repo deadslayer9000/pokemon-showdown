cf033eb9e3b10ab153a6ddbc2785dd691c952079
Guiltybench <alanchiquete9780@gmail.com>
2025-09-02 09:13:42 -0700

    abilities!

--- data/mods/prems3patch/abilities.ts
+++ data/mods/prems3patch/abilities.ts
@@ -1,3 +1,24 @@
 export const Abilities: import('../../../sim/dex-abilities').ModdedAbilityDataTable = {
-
+guarded: {
+    inherit: true,
+    onSetStatus(status, target, source, effect) {
+			if ((effect as Move)?.status) {
+				this.add("-immune", target, "[from] ability: Guarded");
+			}
+			return false;
+		},
+	onTryAddVolatile(status, target) {
+			if (status.id === "yawn") {
+				this.add("-immune", target, "[from] ability: Guarded");
+				return null;
+			}
+			if (status.id === "flinch") return null;
+		},
+    flags: {breakable: 1},
+},
+steamforged: {
+    inherit: true,
+    onSourceModifyDamage() {},
+    flags: {},
+},
 };
