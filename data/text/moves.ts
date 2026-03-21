0892ebfac08c1356fd15f3f3628ed58a459461ad
CreationQ312 <brospondek04@gmail.com>
2026-03-21 22:04:52 +0100

    move desc fix

--- data/text/moves.ts
+++ data/text/moves.ts
@@ -157,7 +157,7 @@ export const MovesText: { [id: IDEntry]: MoveText } = {
 	},
 	annihilate: {
 		name: "Annihilate",
-		shortDesc: "If it hits but doesnt KO an opponent has to recharge.",
+		shortDesc: "If it hits but doesnt KO an opponent it has to recharge.",
 	},
 	amberassault: {
 		name: "Amber Assault",
