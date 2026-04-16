e495a50ad0558011acade821e6032e15326072ed
Guiltybench <alanchiquete9780@gmail.com>
2026-04-15 18:10:40 -0700

    Update scripts.ts

--- data/mods/ddls3patch/scripts.ts
+++ data/mods/ddls3patch/scripts.ts
@@ -2,12 +2,12 @@ export const Scripts: ModdedBattleScriptsData = {
 inherit: 'gen9',
     gen: 9,
     init() {
-      this.modData("Learnsets", "venusaur").sludgewave = ["9M"];
-	  this.modData("Learnsets", "lopunny").cottonguard = ["9M"];
-	  this.modData("Learnsets", "lopunny").drainingkiss = ["9M"];
-	  this.modData("Learnsets", "lopunny").dynamicpunch = ["9M"];
-	  this.modData("Learnsets", "lopunny").machpunch = ["9M"];
-	  this.modData("Learnsets", "lopunny").swordsdance = ["9M"];
-	  this.modData("Learnsets", "lopunny").trailblaze = ["9M"];
+      this.modData("Learnsets", "venusaur").learnset.sludgewave = ["9M"];
+	  this.modData("Learnsets", "lopunny").learnset.cottonguard = ["9M"];
+	  this.modData("Learnsets", "lopunny").learnset.drainingkiss = ["9M"];
+	  this.modData("Learnsets", "lopunny").learnset.dynamicpunch = ["9M"];
+	  this.modData("Learnsets", "lopunny").learnset.machpunch = ["9M"];
+	  this.modData("Learnsets", "lopunny").learnset.swordsdance = ["9M"];
+	  this.modData("Learnsets", "lopunny").learnset.trailblaze = ["9M"];
     },
 };
