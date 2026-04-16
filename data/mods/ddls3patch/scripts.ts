b657c64cffef8da17404a4cf59621ac0544eb633
Guiltybench <alanchiquete9780@gmail.com>
2026-04-15 18:09:21 -0700

    Update scripts.ts

--- data/mods/ddls3patch/scripts.ts
+++ data/mods/ddls3patch/scripts.ts
@@ -1,8 +1,13 @@
 export const Scripts: ModdedBattleScriptsData = {
-    /*gen: 9,
+inherit: 'gen9',
+    gen: 9,
     init() {
       this.modData("Learnsets", "venusaur").sludgewave = ["9M"];
-	  
+	  this.modData("Learnsets", "lopunny").cottonguard = ["9M"];
+	  this.modData("Learnsets", "lopunny").drainingkiss = ["9M"];
+	  this.modData("Learnsets", "lopunny").dynamicpunch = ["9M"];
+	  this.modData("Learnsets", "lopunny").machpunch = ["9M"];
+	  this.modData("Learnsets", "lopunny").swordsdance = ["9M"];
+	  this.modData("Learnsets", "lopunny").trailblaze = ["9M"];
     },
-	*/
 };
