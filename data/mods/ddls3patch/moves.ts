728a238ed9696c92fbc91dde4d34584b1356bebd
Guiltybench <alanchiquete9780@gmail.com>
2026-04-16 17:46:39 -0700

    Create moves.ts

---
+++ data/mods/ddls3patch/moves.ts
@@ -0,0 +1,10 @@
+export const Moves: import('../../../sim/dex-moves').ModdedMoveDataTable = {
+	return: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	frustration: {
+		inherit: true,
+		isNonstandard: null,
+	}
+};
