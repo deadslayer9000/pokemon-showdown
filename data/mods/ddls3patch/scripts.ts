f04ee03512176a43b3ab23fb1c2df4f3e6564213
Guiltybench <alanchiquete9780@gmail.com>
2026-05-02 18:09:46 -0700

    Update scripts.ts

--- data/mods/ddls3patch/scripts.ts
+++ data/mods/ddls3patch/scripts.ts
@@ -1,6 +1,29 @@
 export const Scripts: ModdedBattleScriptsData = {
 inherit: 'gen9',
     gen: 9,
+		pokemon: { //""hardcode"" designed specifically for moonwake because neutralizing gas is hardcoded, e.g. moonwake is now hardcoded e.g. I gotta paste this in to disable that + dawnbreak's immunity to it
+		inherit: true,
+		ignoringAbility() {
+		if (this.battle.gen >= 5 && !this.isActive) return true;
+
+		// Certain Abilities won't activate while Transformed, even if they ordinarily couldn't be suppressed (e.g. Disguise)
+		if (this.getAbility().flags['notransform'] && this.transformed) return true;
+		if (this.getAbility().flags['cantsuppress']) return false;
+		if (this.volatiles['gastroacid']) return true;
+
+		// Check if any active pokemon have the ability Neutralizing Gas
+		if (this.hasItem('Ability Shield') || this.ability === ('neutralizinggas' as ID)) return false;
+		for (const pokemon of this.battle.getAllActive()) {
+			// can't use hasAbility because it would lead to infinite recursion
+			if (pokemon.ability === ('neutralizinggas' as ID) && !pokemon.volatiles['gastroacid'] &&
+				!pokemon.transformed && !pokemon.abilityState.ending && !this.volatiles['commanding']) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+},
     init() {
       this.modData("Learnsets", "venusaur").learnset.sludgewave = ["9M"];
 	  this.modData("Learnsets", "blastoise").learnset.ironhead = ["9M"];
@@ -191,31 +214,8 @@ inherit: 'gen9',
 	  this.modData("Learnsets", "tinkaton").learnset.woodhammer = ["9M"];
 	  //delta changes
 	  this.modData("Learnsets", "grimmsnarldelta").learnset.iciclespear = ["9M"];
-	  this.modData("Learnsets", "keldeodelta").learnset.vacuumwave = undefined;
+	  delete this.modData("Learnsets", "keldeodelta").learnset.vacuumwave;
 	  this.modData("Learnsets", "chansey").learnset.wish = ["9M"];
 	  this.modData("Learnsets", "chansey").learnset.teleport = ["9M"];
 	}
-	/*pokemon: { //""hardcode"" designed specifically for moonwake because neutralizing gas is hardcoded, e.g. moonwake is now hardcoded e.g. I gotta paste this in to disable that + dawnbreak's immunity to it
-		inherit: true,
-		ignoringAbility() {
-		if (this.battle.gen >= 5 && !this.isActive) return true;
-
-		// Certain Abilities won't activate while Transformed, even if they ordinarily couldn't be suppressed (e.g. Disguise)
-		if (this.getAbility().flags['notransform'] && this.transformed) return true;
-		if (this.getAbility().flags['cantsuppress']) return false;
-		if (this.volatiles['gastroacid']) return true;
-
-		// Check if any active pokemon have the ability Neutralizing Gas
-		if (this.hasItem('Ability Shield') || this.ability === ('neutralizinggas' as ID)) return false;
-		for (const pokemon of this.battle.getAllActive()) {
-			// can't use hasAbility because it would lead to infinite recursion
-			if (pokemon.ability === ('neutralizinggas' as ID) && !pokemon.volatiles['gastroacid'] &&
-				!pokemon.transformed && !pokemon.abilityState.ending && !this.volatiles['commanding']) {
-				return true;
-			}
-		}
-
-		return false;
-	}
-}*/
 };
