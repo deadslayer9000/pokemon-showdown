be5028b527524441c4eca85b56eae9d84f245dcf
Guiltybench <alanchiquete9780@gmail.com>
2026-06-07 23:37:26 -0700

    Update formats-data.ts

--- data/mods/chompyvgc/formats-data.ts
+++ data/mods/chompyvgc/formats-data.ts
@@ -5,35 +5,55 @@ export const FormatsData: import('../../../sim/dex-species').ModdedSpeciesFormat
 	},
 	clefablemega: {
 		tier: "OU",
+		isNonstandard: null
 	},
 	victreebelmega: {
 		tier: "OU",
+		isNonstandard: null
 	},
 	dragonitemega: {
 		tier: "OU",
+		isNonstandard: null
 	},
 	chimechomega: {
 		tier: "OU",
+		isNonstandard: null
 	},
 	froslassmega: {
 		tier: "OU",
+		isNonstandard: null
 	},
 	excadrillmega: {
 		tier: "OU",
+		isNonstandard: null
 	},
 	golurkmega: {
 		tier: "OU",
+		isNonstandard: null
 	},
 	meowsticmega: {
 		tier: "OU",
+		isNonstandard: null
 	},
 	crabominablemega: {
 		tier: "OU",
+		isNonstandard: null
 	},
 	scovillainmega: {
 		tier: "OU",
+		isNonstandard: null
 	},
 	glimmoramega: {
 		tier: "OU",
-	}
+		isNonstandard: null
+	},
+	starmiemega: {
+		tier: "OU",
+		isNonstandard: null
+	},
+	drampamega: {
+		tier: "OU",
+		isNonstandard: null
+	},
+
 }
