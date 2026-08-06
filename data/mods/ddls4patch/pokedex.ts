07b6497b375e5e7182b9ce4fa5c5e4dc8d5e82b5
Guiltybench <alanchiquete9780@gmail.com>
2026-08-06 12:42:52 -0700

    Create pokedex.ts

---
+++ data/mods/ddls4patch/pokedex.ts
@@ -0,0 +1,62 @@
+export const Pokedex: import('../../../sim/dex-species').ModdedSpeciesDataTable = {
+	baxcaliburmega: {
+		inherit: true,
+		tier: "Uber",
+		natDexTier: "Uber",
+		abilities: {0: "Thermal Exchange"},
+	},
+	garchompmegaz: {
+		inherit: true,
+		tier: "Uber",
+		natDexTier: "Uber",
+		abilities: {0: "Levitate"},
+	},
+	heatranmega: {
+		inherit: true,
+		tier: "Uber",
+		natDexTier: "Uber",
+		abilities: {0: "Flash Fire"},
+	},
+	darkraimega: {
+		inherit: true,
+		tier: "Uber",
+		natDexTier: "Uber",
+		abilities: {0: "Dark Aura"},
+	},
+	magearnamega: {
+		inherit: true,
+		tier: "Uber",
+		natDexTier: "Uber",
+		abilities: {0: "Sheer Force"},
+	},
+	zeraoramega: {
+		inherit: true,
+		tier: "Uber",
+		natDexTier: "Uber",
+		abilities: {0: "Transistor"},
+	},
+	golisopodmega: {
+		inherit: true,
+		tier: "Uber",
+		natDexTier: "Uber",
+		abilities: {0: "Tough Claws"},
+	},
+	tatsugiricurlymega: {
+		inherit: true,
+		tier: "Uber",
+		natDexTier: "Uber",
+		abilities: {0: "Storm Drain"},
+	},
+	tatsugiridroopymega: {
+		inherit: true,
+		tier: "Uber",
+		natDexTier: "Uber",
+		abilities: {0: "Storm Drain"},
+	},
+	tatsugiristretchymega: {
+		inherit: true,
+		tier: "Uber",
+		natDexTier: "Uber",
+		abilities: {0: "Storm Drain"},
+	},
+};
