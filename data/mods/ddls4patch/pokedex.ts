f9c4e7f32538480bbd74817784240afb27a3fb71
Guiltybench <alanchiquete9780@gmail.com>
2026-08-31 23:27:11 -0700

    ddls4patch

--- data/mods/ddls4patch/pokedex.ts
+++ data/mods/ddls4patch/pokedex.ts
@@ -39,4 +39,20 @@ export const Pokedex: import('../../../sim/dex-species').ModdedSpeciesDataTable 
 		inherit: true,
 		abilities: {0: "Storm Drain"},
 	},
-};
+	grimmeon: {
+		inherit: true,
+		baseStats: {hp: 112, atk: 68, def: 104, spa: 64, spd: 132, spe: 80 },
+	},
+	uxieomega: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 80, def: 120, spa: 80, spd: 120, spe: 100 },
+	},
+	radiantmoon: {
+		inherit: true,
+		baseStats: { hp: 84, atk: 92, def: 76, spa: 132, spd: 124, spe: 100 },
+	},
+	zephior: {
+		inherit: true,
+		baseStats: { hp: 60, atk: 50, def: 80, spa: 110, spd: 70, spe: 130 },
+	},
+};	
